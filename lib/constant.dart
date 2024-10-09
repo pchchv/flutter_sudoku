05b28e39b8e7c09d83d2b866b940391695946b35
pchchv <ipchchv@gmail.com>
2024-10-09 14:05:11 +0300

    Add global constants

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+/// app global constant define here
+class Constant {
+  static const bool enableGoogleFirebase = true;
+  static const String packageName = "com.sevlow.app.sudoku";
+  static const String githubRepository = "https://github.com/pchchv/flutter_sudoku";
+}
