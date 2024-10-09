65b8fe70f784407c4d6d7c1a26a58501e2634a7c
pchchv <ipchchv@gmail.com>
2024-10-09 14:12:54 +0300

    Init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
+import 'dart:ui';
+import 'dart:isolate';
+import 'package:logger/logger.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
+import 'constant.dart';
+import 'firebase_options.dart';
+
+final Logger log = Logger();
 
 void main() {
   runApp(const MyApp());
@@ -6,6 +16,34 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  _firebaseInit() async {
+    if (!Constant.enableGoogleFirebase) {
+      log.i("Google Firebase is disable.");
+      return;
+    }
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    // Flutter Not Catch Error Handler
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+    };
+    // Flutter Async Error Handler
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+    // Flutter Isolate Context Error Handler
+    Isolate.current.addErrorListener(RawReceivePort((pair) async {
+      final List<dynamic> errorAndStacktrace = pair;
+      await FirebaseCrashlytics.instance.recordError(
+        errorAndStacktrace.first,
+        errorAndStacktrace.last,
+        fatal: true,
+      );
+    }).sendPort);
+  }
 
   @override
   Widget build(BuildContext context) {
